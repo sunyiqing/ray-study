6ebc004601b1043442152f571ca2af3dc367cdbd
suneqing <suneqing@163.com>
2018-11-29 18:25:27 +0800

    ADD:logback测试

---
+++ src/main/java/com/ray/sl4j/App.java
@@ -0,0 +1,19 @@
+package com.ray.sl4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+　　* Hello world!
+　　*/
+public class App {
+
+private final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("logback 成功了");
+        logger.error("logback 成功了");
+        logger.warn("logback 成功了");
+        logger.debug("logback 成功了");
+    }
+}
