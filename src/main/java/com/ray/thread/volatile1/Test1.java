d6892e311c340cb046ba12b1eac3641cedfbff75
sunyiqing <sunyiqing@imdada.cn>
2020-08-20 13:47:56 +0800

    volatile Test1

---
+++ src/main/java/com/ray/thread/volatile1/Test1.java
@@ -0,0 +1,34 @@
+package com.ray.thread.volatile1;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  简单 演示 volatile 作用
+ *  1.使用 volatile 时当 running = false,m方法立即结束
+ *  2.不使用volatile 时当 running = false,m方法不会立即结束
+ */
+public class Test1 {
+    volatile boolean running = true;
+//    boolean running = true;
+    void m(){
+        System.out.println("start");
+        while (running){
+            //TODO:
+        }
+        System.out.println("end");
+    }
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        new Thread(test1::m,"t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        test1.running = false;
+        System.out.println("test1.running = false;");
+    }
+
+}
