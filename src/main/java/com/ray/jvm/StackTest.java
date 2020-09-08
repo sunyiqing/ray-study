77c19ff9806c677097bbb50e48e60494d2f57076
suneqing@163.com <suneqing@163.com>
2020-09-09 00:43:42 +0800

    JVM

---
+++ src/main/java/com/ray/jvm/StackTest.java
@@ -0,0 +1,14 @@
+package com.ray.jvm;
+
+/**
+ * Created by yiqing on 2020/9/8.
+ */
+public class StackTest {
+    public static void main(String[] args) {
+        StackTest stackTest = new StackTest();
+        stackTest.test();
+    }
+    public void test(){
+        int b = 1 + 2;
+    }
+}
